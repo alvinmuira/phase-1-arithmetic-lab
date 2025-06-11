689d1b83d76c9bde5e4dd2f8cd58204d0fb30ab2
Alvin Muira <alvinmuira@gmail.com>
2025-06-11 22:30:00 +0300

    completion

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 
 // Write your code here
-
+let num1 = 31;
+let num2 = 2;
 function multiply(num1, num2) {
     return(num1*num2)
 }
 function random() {
     return (Math.random() + 1);
 }
+let num3 = 19;
+let num4 = 5;
 function mod(num3,num4) {
     return (num3 % num4);
 }
 function max() {
     return Math.max(2,4,6,8,10,12,14,16,18,20);
 }
-console.log(multiply(31,2));
+console.log(multiply(num1, num2));
 console.log(random());
-console.log(mod(19,5));
+console.log(mod(num3, num4));
 console.log(max());
