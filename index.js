298f0e0ee7f0211df571dcd4c9dd19c228642414
Alvin Muira <alvinmuira@gmail.com>
2025-06-11 20:11:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 // Write your code here
 let num1 = 31;
 let num2 = 2;
+let num3 = 19;
+let num4 = 5;
 let multiply = num1 * num2;
 let random = Math.random()+1;
+let mod = num3%num4;
 let max = Math.max(20, 5,8,7,12,13);
 console.log(multiply);
 console.log(random);
+console.log(mod);
 console.log(max);
